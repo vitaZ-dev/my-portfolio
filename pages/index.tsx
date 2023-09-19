301f6b55571af2b1567ba635e914c16cdf9460a9
vitaZ-dev <vitaminzdev@gmail.com>
2023-09-19 19:15:45 +0900

    setting: TailwindCSS 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-      <h1>오늘도 화이팅</h1>
-    </main>
+    <div>
+      <h1 className="text-3xl font-bold underline">오늘도 화이팅</h1>
+    </div>
   );
 }
