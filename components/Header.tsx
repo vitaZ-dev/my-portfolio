55af42fbe0a055e80c24bc00973b0e9a0fa0b694
vitaZ-dev <vitaminzdev@gmail.com>
2023-09-27 09:33:52 +0900

    feat: header 작업 중

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Header = () => {
   return (
     <>
-      <header className="text-gray-600 body-font">
+      <header className="fixed w-full bg-white backdrop-opacity-40 text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link
             href="/"
@@ -22,17 +22,17 @@ const Header = () => {
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span className="ml-3 text-xl">My Portfolio</span>
+            <span className="ml-3 text-xl">황지현 포트폴리오</span>
           </Link>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href="/" className="mr-5 hover:text-gray-900 cursor-pointer">
+            <Link href="#" className="mr-5 hover:text-gray-900 cursor-pointer">
               Home
             </Link>
-            <Link href="/projects" className="mr-5 hover:text-gray-900 cursor-pointer">
-              Project
-            </Link>
-            <Link href="/about-me" className="mr-5 hover:text-gray-900 cursor-pointer">
+            <Link href="#about-me" className="mr-5 hover:text-gray-900 cursor-pointer">
               About me
+            </Link>
+            <Link href="#projects" className="mr-5 hover:text-gray-900 cursor-pointer">
+              Project
             </Link>
           </nav>
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
